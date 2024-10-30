e0b69abd4898a868244a519750a14109853d506d
gab1mattos <gabydemattos@icloud.com>
2024-10-30 13:04:40 +0000

    8. Reading Keystrokes

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,14 +1,19 @@
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
+import com. googlecode. lanterna. input. KeyStroke;
 
+import javax.swing.*;
 import java.io.IOException;
 
 public class Game {
     private Screen screen;
+    private int x = 10;
+    private int y = 10;
 
     public Game() {
         try {
@@ -31,15 +36,31 @@ public class Game {
 
     private void draw() throws IOException {
         screen.clear();
-        screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
+        screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
         screen.refresh();
     }
 
     public void run() {
-        try {
-            draw();
-        } catch (IOException e) {
-            e.printStackTrace();
+        while(true) {
+            try {
+                draw();
+                KeyStroke key = screen.readInput();
+                if (key.getKeyType() == KeyType.Character && key.getCharacter() == 'q') screen.close();
+                if (key.getKeyType() == KeyType.EOF) break;
+                processKey(key);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void processKey(KeyStroke key) {
+        System.out.println(key);
+        switch (key.getKeyType()) {
+            case KeyType.ArrowUp -> y--;
+            case KeyType.ArrowDown -> y++;
+            case KeyType.ArrowLeft -> x--;
+            case KeyType.ArrowRight -> x++;
         }
     }
 }
